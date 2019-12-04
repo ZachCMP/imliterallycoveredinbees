b38bc028eece7aca0663ecc34ef9d8c119681576
ZachCMP <zparks88@gmail.com>
2019-12-04 13:53:25 -0500

    spinning bee

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 const port = process.env.PORT || 8080
 
+app.use('/', express.static(path.join(__dirname, 'static')))
+
 app.use('/', (req, res) => {
-  return res.send('TEST')
+  return res.sendFile(path.join(__dirname, 'templates/index.html'))
 })
 
-app.listen(port)
+app.listen(port, () => `App listening on port ${port}`)
